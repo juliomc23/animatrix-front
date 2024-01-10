8fbf9b6157a201a8ef36dfb161b60ed10da3e272
juliomc23 <julito2302mc@gmail.com>
2024-01-10 17:01:38 +0100

    feat: add css for login-form
    This new feature provides CSS for login-form

--- src/app/views/login/components/login-form/login-form.component.ts
+++ src/app/views/login/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 
@@ -6,11 +6,13 @@ import { LoginService } from '../../services/login.service';
   selector: 'app-login-form',
   standalone: true,
   imports: [ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
   private loginService: LoginService = inject(LoginService);
+  watchPassword = false;
   loginFormControl = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
@@ -20,5 +22,9 @@ export class LoginFormComponent {
     const { email, password } = this.loginFormControl.value;
 
     if (email && password) this.loginService.login(email, password);
+  }
+
+  toggleWatchPassword() {
+    this.watchPassword = !this.watchPassword;
   }
 }
