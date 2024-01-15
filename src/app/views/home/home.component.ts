69fe4e91d516d93bae7ac7666c1f655f1c4ab7eb
julio <julito2302mc@gmail.com>
2024-01-15 16:35:25 +0100

    feat: add manga and anime subscription in home component
    this change, provides home with methods to get anime and manga views

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,29 +1,35 @@
+import { SlicePipe } from '@angular/common';
 import { Component, WritableSignal, effect, inject } from '@angular/core';
+import { ResourceCardComponent } from '../../components/resource-card/resource-card.component';
 import { Manga } from '../mangas/interfaces/manga.interface';
 import { MangasService } from '../mangas/services/mangas.service';
-import { MangaCardComponent } from '../mangas/components/manga-card/manga-card.component';
-import { SlicePipe } from '@angular/common';
+import { AnimesService } from '../animes/services/animes.service';
+import { AnimeResponse } from '../animes/interfaces/anime.interface';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MangaCardComponent, SlicePipe],
+  imports: [ResourceCardComponent, SlicePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   private mangasService = inject(MangasService);
+  private animesService = inject(AnimesService);
 
   $mangas: WritableSignal<Manga[]> = this.mangasService.getMangas();
+  $animes: WritableSignal<AnimeResponse[]> = this.animesService.getAnimes();
 
   constructor() {
     effect(() => {
       this.getMangas();
+      this.getAnimes();
     });
   }
 
   ngOnInit(): void {
     this.getMangasSubscription();
+    this.getAnimesSubscription();
   }
 
   getMangasSubscription() {
@@ -35,7 +41,19 @@ export class HomeComponent {
     });
   }
 
+  getAnimesSubscription() {
+    this.animesService.getAnimesSubscription().subscribe({
+      next: (animes) => {
+        this.animesService.setAnimes(animes);
+      },
+    });
+  }
+
   getMangas() {
     this.$mangas = this.mangasService.getMangas();
+  }
+
+  getAnimes() {
+    this.$animes = this.animesService.getAnimes();
   }
 }
