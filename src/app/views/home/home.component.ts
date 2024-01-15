b145f37b8952d18b0591c74336d88500bbca45dc
julio <julito2302mc@gmail.com>
2024-01-15 11:56:56 +0100

    feat: add first 2 mangas for home
    this new change, provides the 2 first mangas to home view

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, effect, inject } from '@angular/core';
+import { Manga } from '../mangas/interfaces/manga.interface';
+import { MangasService } from '../mangas/services/mangas.service';
+import { MangaCardComponent } from '../mangas/components/manga-card/manga-card.component';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MangaCardComponent, SlicePipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  private mangasService = inject(MangasService);
 
+  $mangas: WritableSignal<Manga[]> = this.mangasService.getMangas();
+
+  constructor() {
+    effect(() => {
+      this.getMangas();
+    });
+  }
+
+  ngOnInit(): void {
+    this.getMangasSubscription();
+  }
+
+  getMangasSubscription() {
+    this.mangasService.getMangasSusbcription().subscribe({
+      next: (mangas) => {
+        this.mangasService.setMangas(mangas);
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  getMangas() {
+    this.$mangas = this.mangasService.getMangas();
+  }
 }
